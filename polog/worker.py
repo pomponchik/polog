45447b6098cd83e5ec7deb40a902a25db1a926a7
pomponchik <zheni-b@yandex.ru>
2020-07-19 04:01:48 +0300

    bugfixes

--- polog/worker.py
+++ polog/worker.py
@@ -1,3 +1,4 @@
+import time
 import atexit
 from pony.orm import db_session, commit
 from polog.connector import Connector
@@ -11,8 +12,10 @@ class Worker(object):
     def __init__(self, queue, index):
         self.index = index
         self.queue = queue
+        # Инициализация соединения.
         self.connector = Connector()
-        #метка full нужна, чтобы не завершить поток раньше времени, когда он уже взял таску из очереди, но еще не успел записать ее в БД.
+        # Метка full нужна, чтобы не завершить поток раньше времени, когда он уже взял таску из очереди, но еще не успел записать ее в БД.
+        # По умолчанию метка в фиктивном положительном положении, чтобы исключить ситуацию с преждевременным прекращением потока при очень быстром завершении программы. Если метка по умолчанию будет отрицательной, функция ожидания завершения не будет дожидаться окончания записи и прервет поток на самом интересном месте.
         self.full = True
         self.await_empty_queue()
 
@@ -38,9 +41,15 @@ class Worker(object):
     def await_empty_queue(self):
         """
         Прежде, чем завершить программу, дожидаемся, пока все потоки запишут в базу последние сообщения.
+        Лимит на ожидание - 1 секунда. Он необходим для разрешения ситуации, когда поток еще ни разу не писал в лог сообщений, и соответственно статус заполненности у него фиктивный, в результате чего из него невозможно выйти.
         """
         @atexit.register
         def full_checker():
+            start_awaiting_time = time.time()
             while True:
+                maybe_finish = time.time()
+                time_delta = maybe_finish - start_awaiting_time
+                if time_delta > 1.0:
+                    break
                 if (not self.full) and self.queue.empty():
                     break
