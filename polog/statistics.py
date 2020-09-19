52038da12fbf26a18d3f860ec95ee6e99d08d646
pomponchik <zheni-b@yandex.ru>
2020-09-19 14:29:17 +0300

    readme

--- polog/statistics.py
+++ polog/statistics.py
@@ -1,7 +1,7 @@
 class Statistics:
     def __init__(self, logs):
         if logs is None:
-            raise ValueError('None is not logs!')
+            raise ValueError('None is not logs object!')
         self.logs = logs
 
 
