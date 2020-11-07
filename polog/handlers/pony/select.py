e3e500fb229959c1a9819da2816786a6be214610
pomponchik <zheni-b@yandex.ru>
2020-11-07 23:45:49 +0300

    pony deleted

--- polog/handlers/pony/select.py
+++
@@ -1,7 +0,0 @@
-from pony.orm import db_session, count
-from polog.model import Log
-from polog.handlers.pony.select_items import select_items
-
-
-logs = Log
-select = select_items
--- polog/handlers/pony/statistics.py
+++
@@ -1,5 +0,0 @@
-class Statistics:
-    def __init__(self, logs):
-        if logs is None:
-            raise ValueError('None is not logs object!')
-        self.logs = logs
