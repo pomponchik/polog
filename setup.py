f8921051ce926f169f676f6a44235656260913ec
pomponchik <zheni-b@yandex.ru>
2020-11-12 00:52:35 +0300

    pony has been deleted from the setup file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ['pony==0.7.13', 'ujson==3.0.0']
+requirements = ['ujson==3.0.0']
 
 setup(
     name="polog",
