626fcabfec5c673843e9eec9c39eb3f7139c6db8
Bce-OK <den227___@mail.ru>
2020-12-21 07:03:15 +0300

    добавил коментарии

--- polog/handlers/telegram/sender.py
+++ polog/handlers/telegram/sender.py
@@ -6,9 +6,19 @@ class TelegramSender:
     """
 
     """
-    def __init__(self, token, chat_id, alt = None, text_assembler=None, only_errors=None, filter=None):
+    def __init__(self, token, chat_id, alt=None, text_assembler=None, only_errors=None, filter=None):
         """
+        создание объекта отправки сообщение от телеграм-бота
 
+        Обязательные аргументы:
+        token (str) - токен полученный от FatherBot для инициализации бота который отсылает сообщения с логами.
+        chat_id (int) -  id чата (пользователя телеграм) куда отсылать сообщение.
+
+        Необязательные аргументы:
+        text_assembler (function) - альтернативная функция для генерации текста сообщений. Должна принимать в себя те же аргументы, что метод .__call__() текущего класса и возвращать строковый объект.
+        only_errors (bool) - фильтр на отправку писем. В режиме False (то есть по умолчанию) через него проходят все события. В режиме True - только ошибки, т. е., если это не ошибка, письмо гарантированно отправлено не будет.
+        filter (function) - дополнительный пользовательский фильтр на отправку сообщений. По умолчанию он отсутствует, т. е. отправляются сообщения обо всех событиях, прошедших через фильтр "only_errors" (см. строчкой выше). Пользователь может передать сюда свою функцию, которая должна принимать набор аргументов, аналогичный методу .__call__() текущего класса, и возвращать bool. Возвращенное значение True из данной функции будет означать, что сообщение нужно отправлять, а False - что нет.
+        alt (function) - функция, которая будет выполнена в случае, если отправка сообщения не удалась или запрещена фильтрами. Должна принимать тот же набор аргументов, что и метод .__call__() текущего класса. Возвращаемые значения не используются.
         """
         self.token = token
         self.chat_id = chat_id
@@ -31,7 +41,7 @@ class TelegramSender:
             self.run_alt(args, **kwargs)
 
     def __repr__(self):
-        return f'telegram_sender(message={self.message}, alt={self.alt})'
+        return f'telegram_sender(chat_id={self.chat_id}, alt={self.alt}, text_assembler={self.text_assembler}, only_errors={self.only_errors}, filter={self.filter})'
 
     def send(self, message):
         """
