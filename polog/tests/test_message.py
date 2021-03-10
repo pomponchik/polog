51e620cb3f57614f60a346cb1d9480c811798048
Евгений Блинов <pomponchik@MacBook-Air-Evgenij.local>
2021-03-10 23:40:58 +0300

    tests

--- polog/tests/test_message.py
+++ polog/tests/test_message.py
@@ -3,39 +3,33 @@ import pytest
 from polog import flog, message, field, config
 
 
-@flog(message='base text')
-def normal_function():
-    message('new text', level=5)
-    return True
-
-@flog(message='base text')
-def error_function():
-    try:
-        raise ValueError('exception text')
-    except ValueError as e:
-        message(e=e)
-
-@flog(message='base text')
-def error_function_2():
-    try:
-        raise ValueError('exception text 2')
-    except ValueError as e:
-        message(exception=e)
-
-@flog(message='base text')
-def error_function_3():
-    message(exception_type='ValueError', exception_message='new message')
-
-
 def test_basic(handler):
     """Проверяем, что дефолтное сообщение подменяется новым."""
+    @flog(message='base text')
+    def normal_function():
+        message('new text', level=5)
+        return True
     normal_function()
-    # Дожидаемся, чтобы лог успел записаться.
     time.sleep(0.0001)
     assert handler.last['message'] == 'new text'
 
 def test_basic_exception(handler):
     """Проверяем работу с исключениями."""
+    @flog(message='base text')
+    def error_function():
+        try:
+            raise ValueError('exception text')
+        except ValueError as e:
+            message(e=e)
+    @flog(message='base text')
+    def error_function_2():
+        try:
+            raise ValueError('exception text 2')
+        except ValueError as e:
+            message(exception=e)
+    @flog(message='base text')
+    def error_function_3():
+        message(exception_type='ValueError', exception_message='new message')
     error_function()
     time.sleep(0.0001)
     assert handler.last['exception_type'] == 'ValueError'
