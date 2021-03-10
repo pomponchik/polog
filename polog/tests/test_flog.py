8d76073db3e5d82ae743d1bd7cc5865a675deff6
Евгений Блинов <pomponchik@MacBook-Air-Evgenij.local>
2021-03-10 23:25:22 +0300

    tests

--- polog/tests/test_flog.py
+++ polog/tests/test_flog.py
@@ -6,35 +6,13 @@ from polog.flog import FunctionLogger
 from polog.utils.json_vars import json_one_variable
 
 
-@flog(message='base text')
-def function():
-    return True
-
-@flog(message='base text 2')
-@flog(message='base text')
-def function_2():
-    return True
-
-@flog(message='base text')
-async def function_3():
-    return True
-
-@flog
-def function_4():
-    return True
-
-@flog(level=7)
-def function_5():
-    pass
-
-@flog(level='lolkeklolkeklol')
-def function_6():
-    pass
-
 def test_empty(handler):
     """
     Проверяем, что лог через flog записывается.
     """
+    @flog(message='base text')
+    def function():
+        return True
     handler.clean()
     function()
     time.sleep(0.0001)
@@ -47,18 +25,24 @@ def test_empty_async(handler):
     """
     Проверяем, что лог через flog записывается (для корутин).
     """
+    @flog(message='base text')
+    async def function():
+        return True
     handler.clean()
-    asyncio.run(function_3())
+    asyncio.run(function())
     time.sleep(0.0001)
     log = handler.last
     assert log is not None
-    assert log['module'] == function_3.__module__
-    assert log['function'] == function_3.__name__
+    assert log['module'] == function.__module__
+    assert log['function'] == function.__name__
 
 def test_message(handler):
     """
     Проверяем, что сообщение по умолчанию записывается.
     """
+    @flog(message='base text')
+    def function():
+        return True
     handler.clean()
     function()
     time.sleep(0.0001)
@@ -69,8 +53,12 @@ def test_double(handler):
     """
     Проверка, что при двойном декорировании запись генерится только одна.
     """
+    @flog(message='base text 2')
+    @flog(message='base text')
+    def function():
+        return True
     handler.clean()
-    function_2()
+    function()
     time.sleep(0.0001)
     assert len(handler.all) == 1
     assert handler.last['message'] == 'base text 2'
@@ -79,19 +67,28 @@ def test_working():
     """
     Проверяем, что декоратор не ломает поведение функции.
     """
+    @flog(message='base text')
+    def function():
+        return True
     assert function() == True
 
 def test_working_without_brackets():
     """
     Проверяем, декоратор можно вызывать без скобок и функция остается рабочей.
     """
-    assert function_4() == True
+    @flog
+    def function():
+        return True
+    assert function() == True
 
 def test_level(handler):
     """
     Проверяем, что установка уровня логирования работает.
     """
-    function_5()
+    @flog(level=7)
+    def function():
+        pass
+    function()
     time.sleep(0.0001)
     assert handler.last['level'] == 7
 
@@ -99,12 +96,15 @@ def test_level(handler):
     """
     Проверяем, что установка уровня логирования идентификатором в виде строки работает.
     """
+    @flog(level='lolkeklolkeklol')
+    def function():
+        pass
     config.levels(lolkeklolkeklol=88)
-    function_6()
+    function()
     time.sleep(0.0001)
     assert handler.last['level'] == 88
     config.levels(lolkeklolkeklol=77)
-    function_6()
+    function()
     time.sleep(0.0001)
     assert handler.last['level'] == 77
 
