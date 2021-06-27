1bab79c288a32a93c9b9a1169968e8f924fd4a84
pomponchik <zheni-b@yandex.ru>
2021-06-27 23:01:54 +0300

    tests

--- polog/tests/core/stores/settings/test_settings_store.py
+++ polog/tests/core/stores/settings/test_settings_store.py
@@ -4,7 +4,7 @@ from polog.core.stores.settings.settings_store import SettingsStore
 
 def test_set_and_get():
     """
-    Проверяем, что назначение новых параметров через .__init__() работает.
+    Проверяем, что параметры назначаются.
     """
     all_values = [
         {
@@ -30,3 +30,21 @@ def test_set_and_get():
         for key, value in values.items():
             store[key] = value
             assert store[key] == value
+
+def test_set_error_values():
+    """
+    Проверяем, что при попытке изменить значение параметра на невозможный поднимается исключение.
+    """
+    all_values = {
+        'pool_size': [1.4, None, 'kek', -100],
+        'original_exceptions': [None, 'no', 7, 1.5],
+        'level': [1.2, None, -6],
+        'service_name': [1.2, None],
+        'errors_level': [1.2, None, -5],
+        'delay_before_exit': ['kek', None, -1],
+    }
+    store = SettingsStore()
+    for key, local_values in all_values.items():
+        for value in local_values:
+            with pytest.raises(ValueError):
+                store[key] = value
