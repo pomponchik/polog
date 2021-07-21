061495e271de5c4fbd1db2e0307aae797ac44c06
pomponchik <zheni-b@yandex.ru>
2021-07-21 11:56:35 +0300

    test

--- polog/tests/core/engine/test_engine.py
+++ polog/tests/core/engine/test_engine.py
@@ -1,4 +1,5 @@
-from threading import active_count
+import time
+from threading import active_count, Thread
 import pytest
 from polog.core.engine.engine import Engine
 from polog.core.stores.settings.settings_store import SettingsStore
@@ -24,3 +25,28 @@ def test_reload_threads_counting():
     after = active_count()
 
     assert after == before + 2
+
+def test_reload_missive_attack(handler):
+    """
+    Пробуем перезагрузить движок в тот момент, пока другой поток пишет логи.
+    За счет блокировок ни одно событие не должно потеряться.
+    """
+    engine = Engine()
+    store = SettingsStore()
+    engine.write(None, **{'lol': 'kek'})
+    handler.clean()
+
+    number_of_items = 3000
+
+    def go_attack():
+        for x in range(number_of_items):
+            engine.write((None, None), **{'lol': 'kek'})
+    thread = Thread(target=go_attack)
+    thread.start()
+
+    engine.reload()
+    time.sleep(0.1)
+
+    assert len(handler.all) == number_of_items
+
+    thread.join()
