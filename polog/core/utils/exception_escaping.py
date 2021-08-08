fe1a5bfe07873007a8ada395895a42a4f0d0ac4d
pomponchik <zheni-b@yandex.ru>
2021-08-08 15:02:58 +0300

    docstring

--- polog/core/utils/exception_escaping.py
+++ polog/core/utils/exception_escaping.py
@@ -2,6 +2,11 @@ from functools import wraps
 
 
 def exception_escaping(func):
+    """
+    Декоратор для подавления любых исключений.
+
+    Применять с осторожностью и только по отношению к пользовательским функциям. Недопустимо экранировать внутренние ошибки Polog.
+    """
     @wraps(func)
     def wrapper(*args, **kwargs):
         try:
