eafeca615c774042ff11774e8fa0785ff1b1bb8d
pomponchik <zheni-b@yandex.ru>
2021-08-11 17:08:38 +0300

    SingleThreadedRealEngine + tests

--- polog/core/engine/real_engines/singlethreaded/engine.py
+++ polog/core/engine/real_engines/singlethreaded/engine.py
@@ -1,9 +1,21 @@
 from polog.core.engine.real_engines.abstract import AbstractRealEngine
+from polog.core.utils.exception_escaping import exception_escaping
 
 
 class SingleThreadedRealEngine(AbstractRealEngine):
     """
     Однопоточная синхронная реализация движка.
     """
-    def stop(self):
-        pass
+    def write(self, function_input_data, **fields):
+        """
+        Передаем данные о событии в обработчики.
+        """
+        for handler in self.settings.handlers.values():
+            self.call_handler(handler, function_input_data, fields)
+
+    @exception_escaping
+    def call_handler(self, handler, function_input_data, fields):
+        """
+        Вызов одного обработчика.
+        """
+        handler(function_input_data, **fields, service_name=self.settings['service_name'])
