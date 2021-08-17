fdcaebd42146a1d2a3c3ab81da8192ca33ff9cd2
pomponchik <zheni-b@yandex.ru>
2021-08-17 15:50:45 +0300

    docstrings

--- polog/core/engine/real_engines/multithreaded/pool.py
+++ polog/core/engine/real_engines/multithreaded/pool.py
@@ -4,12 +4,22 @@ from queue import Queue
 
 
 class ThreadPool:
+    """
+    Группа потоков-воркеров, обрабатывающая запись логов. Каждый воркер смотрит в общую очередь и забирает оттуда данные.
+
+    Группа поддерживает команды, относящиеся ко всей группе сразу:
+    .put(<данные лога>) - записать лог.
+    .stop() - дождаться записи всех логов в очереди, после чего завершить потоки-воркеры и присоединить их.
+    """
     def __init__(self, settings):
         self.settings = settings
         self.queue = Queue(maxsize=self.settings.force_get('max_queue_size'))
         self.workers = self.create_workers()
 
     def put(self, function_input_data, **fields):
+        """
+        Помещение лога в очередь.
+        """
         self.queue.put(function_input_data, **fields)
 
     def stop(self):
@@ -30,6 +40,13 @@ class ThreadPool:
             worker.stop()
 
     def wait_empty_queue(self):
+        """
+        Опрашиваем очередь с некоторой периодичностью, пока она не опустеет. Когда очередь опустела, выходим.
+
+        Периодичность опроса определяется двумя пунктами настроек - 'time_quant' и 'delay_on_exit_loop_iteration_in_quants'.
+        'time_quant' - временной квант, константа, определяющая минимальное время выполнения любой операции (в секундах).
+        'delay_on_exit_loop_iteration_in_quants' - количество квантов (целое число), на которое мы засыпаем в каждой итерации цикла опроса.
+        """
         delay = self.settings['time_quant'] * self.settings['delay_on_exit_loop_iteration_in_quants']
         while True:
             if self.queue.empty():
@@ -37,6 +54,9 @@ class ThreadPool:
             time.sleep(delay)
 
     def create_workers(self):
+        """
+        Создание и запуск воркеров.
+        """
         workers = []
         for index in range(self.settings.force_get('pool_size')):
             worker = Worker(self.queue, index, self.settings)
