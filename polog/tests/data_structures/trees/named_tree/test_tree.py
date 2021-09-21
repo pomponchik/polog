2f068dc80167d52688d84cd7f998c8cd62910bab
pomponchik <zheni-b@yandex.ru>
2021-09-22 00:27:09 +0300

    test

--- polog/tests/data_structures/trees/named_tree/test_tree.py
+++ polog/tests/data_structures/trees/named_tree/test_tree.py
@@ -3,8 +3,11 @@ from polog.data_structures.trees.named_tree.tree import NamedTree
 
 
 def test_set_and_get():
+    """
+    Сохраняем значения в дерево и получаем их обратно по ключам.
+    """
     tree = NamedTree()
     tree['kek'] = 'lol'
     assert tree['kek'] == 'lol'
-    tree['kek.cheburek'] = 'lol'
-    assert tree['kek.cheburek'] == 'lol'
+    tree['kek.cheburek'] = 'lolkek'
+    assert tree['kek.cheburek'] == 'lolkek'
