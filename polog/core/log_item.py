94ab4b060416ac449abc17efb5ae30f13081b7fa
pomponchik <zheni-b@yandex.ru>
2021-10-13 20:50:08 +0300

    docstrings

--- polog/core/log_item.py
+++ polog/core/log_item.py
@@ -57,18 +57,28 @@ class LogItem:
         return f'<log item #{id(self)} with content: {content}>'
 
     def __iter__(self):
+        """
+        Получаем итератор по ключам словаря, хранящего в себе поля лога.
+        """
         try:
             return iter(self.fields)
         except AttributeError:
             return iter(())
 
     def __contains__(self, key):
+        """
+        Проверяем, что в логе присутствует поле, чье имя передано в качестве аргумента.
+        Метод срабатывает при использовании оператора 'in'.
+        """
         try:
             return key in self.fields
         except AttributeError:
             return False
 
     def __len__(self):
+        """
+        Получаем количество полей лога.
+        """
         try:
             return len(self.fields)
         except AttributeError:
@@ -111,6 +121,13 @@ class LogItem:
         return self.compare_or_raise(other, lambda a, b: a >= b, '>=')
 
     def compare(self, other, operation, if_not=False):
+        """
+        Сравнение двух экземпляров текущего класса.
+
+        other - другой объект, с которым происходит сравнение.
+        operation - функция, которая должна принимать 2 аргумента и возвращать значение типа bool, которое и будет использовано в качестве результата сравнения.
+        if_not - значение, которое будет возвращено, если сравнение по какой-то причине невозможно.
+        """
         if isinstance(other, type(self)):
             ts_1 = self.get('time')
             ts_2 = other.get('time')
@@ -122,6 +139,13 @@ class LogItem:
         return if_not
 
     def compare_or_raise(self, other, operation, compare_symbol):
+        """
+        Сравнение двух экземпляров текущего класса. В случае невозможности сравнения, поднимается TypeError.
+
+        other - другой объект, с которым происходит сравнение.
+        operation - функция, которая должна принимать 2 аргумента и возвращать значение типа bool, которое и будет использовано в качестве результата сравнения.
+        compare_symbol - строка, изображающая оператор сравнения, которая используется для формирования строки с ошибкой в поднимаемом исключении.
+        """
         if isinstance(other, type(self)):
             ts_1 = self.get('time')
             ts_2 = other.get('time')
@@ -135,18 +159,27 @@ class LogItem:
         raise TypeError(f"'{compare_symbol}' not supported between instances of '{type(self).__name__}' and '{type(other).__name__}'")
 
     def items(self):
+        """
+        Получаем коллекцию, содержащую кортежи, в каждом из которых первый элемент - название поля лога, второй - значение этого поля.
+        """
         try:
             return self.fields.items()
         except AttributeError:
             return ()
 
     def keys(self):
+        """
+        Получаем коллекцию названий полей лога.
+        """
         try:
             return self.fields.keys()
         except AttributeError:
             return ()
 
     def values(self):
+        """
+        Получаем коллекцию значений всех полей лога.
+        """
         try:
             return self.fields.values()
         except AttributeError:
@@ -154,23 +187,30 @@ class LogItem:
 
     @property
     def function_input_data(self):
+        """
+        Получаем объект класса FunctionInputData, содержащий args и kwargs, то есть кортеж с неименованными аргументами функции, из декоратора которой был записан лог, и словарь с именованными аргументами ее же.
+        В случае, если лог записан не с помощью такого декоратора, поля объекта FunctionInputData будут заполнены None.
+        """
         if hasattr(self, '_function_input_data'):
             return self._function_input_data
         return FunctionInputData()
 
-    @function_input_data.setter
-    def function_input_data(self, value):
-        if not isinstance(value, FunctionInputData):
-            raise ValueError('Incorrect data type of the value.')
-        self._function_input_data = value
-
     def get(self, key, *default):
+        """
+        Получаем значение поля лога по его названию. Метод работает аналогично одноименному у словаря.
+        """
         try:
             return self.fields.get(key, *default)
         except AttributeError:
             return {}.get(key, *default)
 
     def set_data(self, data):
+        """
+        Сохраняем словарь с данными в объекте лога.
+
+        По своей сути объект LogItem - это обертка над словарем. Сначала нужно собрать данные лога в словаре, после чего передать этот словарь в объект лога через данный метод.
+        После получения словаря с данными лога, объект LogItem будет проксировать к нему доступ.
+        """
         self.fields = data
 
     def set_function_input_data(self, args, kwargs):
@@ -187,12 +227,20 @@ class LogItem:
         То есть args и kwargs - это, соответственно, кортеж и словарь с позиционными и именованными аргументами функции.
         Обработчики могут использовать эти данные только в редких случаях и на свой страх и риск, поскольку существует возможность повредить данные функции.
         """
-        self.function_input_data = FunctionInputData(args=args, kwargs=kwargs)
+        self._function_input_data = FunctionInputData(args=args, kwargs=kwargs)
 
     def set_handlers(self, handlers):
+        """
+        Сохраняем коллекцию обработчиков в объекте лога.
+
+        Поскольку для каждого отдельного события может быть предусмотрен разный набор обработчиков, они все передаются в объект лога в виде коллекции. Непосредственно в движке эта коллекция затем будет извлечена и лог будет передан в каждый из обработчиков.
+        """
         self._handlers = handlers
 
     def get_handlers(self):
+        """
+        Получаем коллекцию обработчиков, в которую нужно передать данный объект лога.
+        """
         try:
             return self._handlers
         except AttributeError:
