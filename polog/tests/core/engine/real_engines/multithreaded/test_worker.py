d8c337ba9396f521dcfc3425c3d296bc37ab3f16
pomponchik <zheni-b@yandex.ru>
2021-10-21 20:54:15 +0300

    fixed 1 test

--- polog/tests/core/engine/real_engines/multithreaded/test_worker.py
+++ polog/tests/core/engine/real_engines/multithreaded/test_worker.py
@@ -5,6 +5,7 @@ import pytest
 
 from polog.core.engine.real_engines.multithreaded.worker import Worker
 from polog.core.stores.settings.settings_store import SettingsStore
+from polog.core.log_item import LogItem
 
 
 queue = Queue()
@@ -16,7 +17,12 @@ def test_do(handler):
     Проверяем, что хендлер вызывается.
     """
     handler.clean()
-    worker.do_anything({'lol': 'kek'})
+    log = LogItem()
+    log.set_handlers([handler])
+    log.set_data({'lol': 'kek'})
+
+    worker.do_anything(log)
     assert handler.last is not None
+    
     worker.set_stop_flag()
     worker.stop()
