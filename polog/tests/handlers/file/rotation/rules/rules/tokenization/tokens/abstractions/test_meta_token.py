daec80fef09e5e53eda787c08131137cd023a3e6
pomponchik <zheni-b@yandex.ru>
2021-10-28 20:49:09 +0300

    new tests

---
+++ polog/tests/handlers/file/rotation/rules/rules/tokenization/tokens/abstractions/test_meta_token.py
@@ -0,0 +1,41 @@
+import pytest
+
+from polog.handlers.file.rotation.rules.rules.tokenization.tokens.abstractions.meta_token import MetaToken
+
+
+def test_raise_errors_in_metatoken():
+    """
+    Пробуем создавать "неправильные" классы на основе метакласса. Должны подниматься исключения.
+    """
+    with pytest.raises(AttributeError):
+        class TestToken(metaclass=MetaToken):
+            regexp_letter = '['
+
+    with pytest.raises(AttributeError):
+        class TestToken(metaclass=MetaToken):
+            regexp_letter = ']'
+
+    with pytest.raises(AttributeError):
+        class TestToken(metaclass=MetaToken):
+            regexp_letter = '*'
+
+    with pytest.raises(AttributeError):
+        class TestToken(metaclass=MetaToken):
+            regexp_letter = 'lol'
+
+    with pytest.raises(AttributeError):
+        class TestToken(metaclass=MetaToken):
+            pass
+
+    with pytest.raises(AttributeError):
+        class TestToken(metaclass=MetaToken):
+            regexp_letter = True
+
+    for letter in 'ns.':
+        with pytest.raises(AttributeError):
+            class TestToken(metaclass=MetaToken):
+                regexp_letter = letter
+
+    with pytest.raises(AttributeError):
+        class AbstractToken(metaclass=MetaToken):
+            regexp_letter = 'k'
--- polog/tests/handlers/file/rotation/rules/rules/tokenization/tokens/test_dot_token.py
+++ polog/tests/handlers/file/rotation/rules/rules/tokenization/tokens/test_dot_token.py
@@ -3,6 +3,7 @@ import pytest
 from polog.handlers.file.rotation.rules.rules.tokenization.tokens.dot_token import DotToken
 from polog.handlers.file.rotation.rules.rules.tokenization.tokens.number_token import NumberToken
 
+
 def test_equal_to_dot_token():
     """
     Проверяем, что проверка на равенство универсального токена работает корректно.
