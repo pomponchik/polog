299e9085fac6af789e93b63f02a42a5f8b7899b6
pomponchik <zheni-b@yandex.ru>
2022-09-18 20:56:04 +0300

    the new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = []
 
 setup(
     name="polog",
-    version="0.0.13",
+    version="0.0.14",
     author="Evgeniy Blinov",
     author_email="zheni-b@yandex.ru",
     description="Логирование должно быть красивым",
