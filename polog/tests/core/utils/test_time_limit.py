1a78a230e40b58a80a8cc645101dbfd003fc1ba5
pomponchik <zheni-b@yandex.ru>
2022-11-10 17:50:35 +0300

    test

--- polog/tests/core/utils/test_time_limit.py
+++ polog/tests/core/utils/test_time_limit.py
@@ -15,11 +15,11 @@ def test_integer():
     wrapper = time_limit(quant)
     wrapped_function = wrapper(function)
     with pytest.raises(TimeoutError):
-        wrapped_function(5)
+        wrapped_function(2)
 
     wrapper = time_limit(quant * 10)
     wrapped_function = wrapper(function)
-    wrapped_function(5)
+    wrapped_function(2)
 
 def test_function_as_parameter():
     """
