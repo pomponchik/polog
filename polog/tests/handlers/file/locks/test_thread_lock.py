3dcdad6d66b8b20a206d3b86966c9bd8a1b680d6
pomponchik <zheni-b@yandex.ru>
2022-11-10 15:15:22 +0300

    test

--- polog/tests/handlers/file/locks/test_thread_lock.py
+++ polog/tests/handlers/file/locks/test_thread_lock.py
@@ -43,6 +43,29 @@ def test_threads_race_condition_lock_off():
     def incrementer(iterations):
         nonlocal index
         for _ in range(iterations):
+            lock.acquire()
+            index += 1
+            lock.release()
+
+    threads = [Thread(target=incrementer, args=(iterations, )) for thread_index in range(number_of_threads)]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+    assert index < iterations * number_of_threads
+
+def test_threads_race_condition_without_locks():
+    """
+    Проверяем, что состояние гонки возникает без использования локов.
+    """
+    iterations = 500000
+    number_of_threads = 4
+
+    index = 0
+    def incrementer(iterations):
+        nonlocal index
+        for _ in range(iterations):
             index += 1
 
     threads = [Thread(target=incrementer, args=(iterations, )) for thread_index in range(number_of_threads)]
