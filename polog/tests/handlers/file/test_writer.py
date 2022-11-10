fdd009aa7fa2ac4d5dedaf47a95a0d1c303c8afb
pomponchik <zheni-b@yandex.ru>
2022-11-10 23:26:40 +0300

    test

--- polog/tests/handlers/file/test_writer.py
+++ polog/tests/handlers/file/test_writer.py
@@ -13,7 +13,7 @@ from polog.handlers.file.writer import file_writer
 from polog.core.utils.exception_escaping import exception_escaping
 
 
-TIMEOUT = 6
+TIMEOUT = 10
 
 
 def test_base_writer(number_of_strings_in_the_files, delete_files):
