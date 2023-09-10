468a9d58cf1dc06a74f80cf61a715593559c7ddc
pomponchik <zheni-b@yandex.ru>
2023-09-11 01:16:59 +0300

    quotation marks

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding='utf8') as readme_file:
+with open('README.md', 'r', encoding='utf8') as readme_file:
     readme = readme_file.read()
 
 requirements = []
 
 setup(
-    name="polog",
-    version="0.0.17",
-    author="Evgeniy Blinov",
-    author_email="zheni-b@yandex.ru",
-    description="The new generation logger",
+    name='polog',
+    version='0.0.17',
+    author='Evgeniy Blinov',
+    author_email='zheni-b@yandex.ru',
+    description='The new generation logger',
     long_description=readme,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pomponchik/polog",
+    long_description_content_type='text/markdown',
+    url='https://github.com/pomponchik/polog',
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
-        "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: MIT License",
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
     ],
 )
